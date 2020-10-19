596871010ddbed6eba18d02cce307d22505d3e94
ngkong <ngkongs@gmail.com>
2020-10-19 15:58:27 +0700

    prevent goerli pop

--- assets/js/metamask-goerli.js
+++ assets/js/metamask-goerli.js
@@ -47,11 +47,11 @@ var connectMetamask = async function(){
 	
 	//force testnet
 	if(eth_chainId=='0x1'||eth_chainId=='0x01'){ //mainnet
-		//~ Swal.fire(
-		  //~ 'Error',
-		  //~ 'Saving and Lending app is under development and currently only available in Goerli Testnet. Change your Metamask network to Goerli to use this app.',
-		  //~ 'error'
-		//~ );
+		Swal.fire(
+		  'Error',
+		  'Saving and Lending app is under development and currently only available in Goerli Testnet. Change your Metamask network to Goerli to use this app.',
+		  'error'
+		);
 		return;
 	}
 	
@@ -107,6 +107,6 @@ ethereum.on('chainChanged', async (chainId) => {
 
 $(function(){
 	if(typeof ethereum!== 'undefined'){
-		connectMetamask();
+		//connectMetamask();
 	}
 });
