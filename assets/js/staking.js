11580ea26c304b0dda3c441dddaaee5e3529e7c9
ngkong <ngkongs@gmail.com>
2020-11-13 11:33:18 +0700

    fix lpten to lptad

--- assets/js/staking.js
+++ assets/js/staking.js
@@ -405,7 +405,7 @@ var addLpToMetamask = async function(){
 	  type: 'ERC20', // Initially only supports ERC20, but eventually more!
 	  options: {
 		address: ENV.lpAddress, // The address that the token is at.
-		symbol: 'LPTEN', // A ticker symbol or shorthand, up to 5 chars.
+		symbol: 'LPTAD', // A ticker symbol or shorthand, up to 5 chars.
 		decimals: 18, // The number of decimals in the token
 		image: '', // A string url of the token logo
 	  },
